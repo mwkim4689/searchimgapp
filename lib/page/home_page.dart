b5a648c4c05eabdfb1f04a7bc0f8d14a28983aca
kmw <striver89@gmail.com>
2024-04-02 14:23:03 +0900

    홈페이지 서치 로딩 방식 스택으로 변경

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -34,69 +34,83 @@ class _HomePageState extends State<HomePage> {
       onTap: _onTapOutside,
       child: SafeArea(
         child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
+          body: GetBuilder<HomeController>(builder: (_) {
+            return Stack(
               children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    color: Colors.black.withOpacity(0.05),
-                  ),
-                  child: Row(
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
                     children: [
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: TextFormField(
-                          controller: homeController.searchTextController,
-                          textInputAction: TextInputAction.search,
-                          onFieldSubmitted: (String text) {
-                            homeController.search(isInitialSearch: true);
-                          },
-                          onChanged: (String text) {
-                            homeController.setSearchText(text);
-                          },
-                          decoration: InputDecoration(
-                            hintText: "검색어를 입력하세요",
-                            hintStyle: TextStyle(
-                                color: Colors.black.withOpacity(0.35)),
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                          ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          color: Colors.black.withOpacity(0.05),
+                        ),
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: TextFormField(
+                                controller: homeController.searchTextController,
+                                textInputAction: TextInputAction.search,
+                                onFieldSubmitted: (String text) {
+                                  homeController.search(isInitialSearch: true);
+                                },
+                                onChanged: (String text) {
+                                  homeController.setSearchText(text);
+                                },
+                                decoration: InputDecoration(
+                                  hintText: "검색어를 입력하세요",
+                                  hintStyle: TextStyle(
+                                      color: Colors.black.withOpacity(0.35)),
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  errorBorder: InputBorder.none,
+                                  disabledBorder: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            // if (_.searchRecords.isNotEmpty || _.searchText.isNotEmpty)
+                            InkWell(
+                              onTap: () {
+                                homeController.clearSearchText();
+                              },
+                              child: const Icon(Icons.close, size: 20),
+                            ),
+                          ],
                         ),
                       ),
-                      // if (_.searchRecords.isNotEmpty || _.searchText.isNotEmpty)
-                      InkWell(
-                        onTap: () {
-                          homeController.clearSearchText();
-                        },
-                        child: const Icon(Icons.close, size: 20),
-                      ),
+                      const SizedBox(height: 6),
+                      Expanded(
+                        child: ListView.separated(
+                          controller: _.scrollController,
+                          itemCount: _.documentList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return documentItem(_, index);
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const SizedBox(height: 20);
+                          },
+                        ),
+                      )
                     ],
                   ),
                 ),
-                const SizedBox(height: 6),
-                Expanded(
-                  child: GetBuilder<HomeController>(builder: (_) {
-                    return ListView.separated(
-                      controller: _.scrollController,
-                      itemCount: _.documentList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return documentItem(_, index);
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const SizedBox(height: 20);
-                      },
-                    );
-                  }),
-                )
+                if (_.loading == true)
+                  Positioned.fill(
+                    child: Container(
+                      color: Colors.white.withOpacity(0.2),
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  ),
               ],
-            ),
-          ),
+            );
+          }),
         ),
       ),
     );
