0710a74149d5bfa8e38559b2f1303de8b2cfe05d
kmw <striver89@gmail.com>
2024-04-03 22:14:16 +0900

    MainTab에서 GetBuilder 감싸는 범위 Scaffold 로 재수정

--- lib/main_tab.dart
+++ lib/main_tab.dart
@@ -11,34 +11,37 @@ class MainTab extends StatefulWidget {
 }
 
 class _MainTabState extends State<MainTab> {
+
   MainTabController mainTabController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GetBuilder<MainTabController>(builder: (_) {
-        return _.tabs[_.pageIndex];
-      }),
-      bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels: true,
-        selectedFontSize: 11,
-        unselectedFontSize: 11,
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "검색",
+    return GetBuilder<MainTabController>(
+      builder: (_) {
+        return Scaffold(
+          body: _.tabs[_.pageIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            showUnselectedLabels: true,
+            selectedFontSize: 11,
+            unselectedFontSize: 11,
+            type: BottomNavigationBarType.fixed,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "검색",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star_border_rounded),
+                label: "즐겨찾기",
+              ),
+            ],
+            currentIndex: mainTabController.pageIndex,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: const Color(0xFFCBCBCB),
+            onTap: mainTabController.setPageIndex,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star_border_rounded),
-            label: "즐겨찾기",
-          ),
-        ],
-        currentIndex: mainTabController.pageIndex,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: const Color(0xFFCBCBCB),
-        onTap: mainTabController.setPageIndex,
-      ),
+        );
+      }
     );
   }
 }
